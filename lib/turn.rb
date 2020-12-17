3dc62d198eca9d4c506ba13162110964ddb65b0e
Milan Mody <milanm1120@gmail.com>
2020-12-17 00:06:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,19 @@ end
 def valid_move?(board, position, char = "X")
   board[position] = char
 end
+
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else
+    return false
+  end
+end
