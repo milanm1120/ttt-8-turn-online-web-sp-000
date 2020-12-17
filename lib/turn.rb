179e1141e720f6b7f0536d24b614816f567198c4
Milan Mody <milanm1120@gmail.com>
2020-12-17 00:44:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,4 +38,9 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input=gets.strip
   input_to_index(user_input)
+  if position_taken?(board, position) == false && (position).between?(0,8)
+    return true
+  else
+    return false
+  end
 end
