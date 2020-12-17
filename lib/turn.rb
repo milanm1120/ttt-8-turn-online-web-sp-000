e9ac9567b33b1ebcd1a79525bfaf53d026db7307
Milan Mody <milanm1120@gmail.com>
2020-12-17 00:02:46 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index) == false && (index).between?(0,8)
+  if !position_taken?(board, index) && (index).between?(0,8)
     return true
   else
     return false
